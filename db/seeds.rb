49d6df13adfb5f912c5741f337f6c733e61ca460
Mark Harrison <mhworld11@icloud.com>
2019-05-08 10:54:37 -0700

    adds products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.create(name: "Sony", price: 999.00, image_url: " ", description: "32' LCD" )
+product = Product.create(name: "Samsung", price: 1999.00, image_url: " ", description: "52' LCD" )
