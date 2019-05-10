5bd93a9a27773919cd63e55051fb289e0e06d567
Mark Harrison <mhworld11@icloud.com>
2019-05-10 12:06:11 -0700

    adds index, create, and show actions

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -15,9 +15,27 @@ class Api::ProductsController < ApplicationController
     @greeting = "Pick a product from the database"
     @list = Product.all
     @answer = params[:product]
-
-
-
+    @finish = "You've picked a #{@answer}"
     render 'any_product.json.jbuilder'
+  end
+
+  def index
+    @products = Product.all
+    render 'index.json.jbuilder'  
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name], 
+      price: params[:price], 
+      image_url: params[:image_url],
+      description: params[:description])
+    @product.save
+    render 'show.json.jbuilder'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
-  # STEP 1: A ROUTE triggers a controller action
-  # verb "/urls" => "namespace/controllers#action"
+
   namespace :api do
+
     get "one_product" => "products#display_product"
     get "all_products" => "products#display_all"
 
     get "any_product_url/:product" => "products#any_product"
+
+    get "/products" => "products#index"
+
+    get "/products/:id" => "products#show"
+
+    post "/products" => "products#create"
+
+    
   end
+
+end
 
 
 
   
-end
+
